cd0751d6951fe8e4f8d6ac46478fbcde94848d14
Monir Hossain <sgtmonirh@gmail.com>
2020-05-16 00:40:30 +0600

    Home page Design

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+( function( $ ) {
+
+	$('#year').text(new Date().getFullYear());
+	$('#year1').text(new Date().getFullYear());
+	$('#year2').text(new Date().getFullYear());
+	$('#year3').text(new Date().getFullYear());
+	$('#year4').text(new Date().getFullYear());
+	$('#year5').text(new Date().getFullYear());
+
+} )( jQuery );
